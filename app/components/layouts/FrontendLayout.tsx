cd7d4eed2b21e65c58652690255c04fbc0579736
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-06-25 19:59:06 +0530

    Setup and Navbar

---
+++ app/components/layouts/FrontendLayout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function FrontendLayout({children}:{children:ReactNode}){
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import FrontendLayout from "./components/layouts/FrontendLayout";
+
+export default function Home() {
+  return (
+    <FrontendLayout>
+      <section className="relative flex min-h-screen items-center overflow-hidden bg-[url('/data/hero.jpg')] bg-cover bg-center
+      pt-32 lg:pt-36 py-2">
+        {/* overlay */}
+        <div className="absolute inset-0 bg-black/55"/>
+        {/* gradient */}
+        <div className="absolute inset-0 bg-linear-to-r from-slate-950/80 via-slate-900/50 to-transparent"/> 
+      </section>
+    </FrontendLayout>
+  );
+}
