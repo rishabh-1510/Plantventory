bbb6d9466226e972b8acb57fca192649f266926f
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-06-26 14:59:23 +0530

    Navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,24 +1,29 @@
+"use client"
 import Link from "next/link";
 import { Button } from "../ui/Button";
+import { FaHome } from "react-icons/fa";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 interface NavbarProps {
-    variant:"transparent" | "Solid"
+    variant: "transparent" | "Solid"
 }
 
-const navLinks = ["Home","Properties","MarketPlace"];
+const navLinks = ["Home", "Properties", "MarketPlace"];
 
-export const Navbar = ({variant="transparent"}:NavbarProps) => {
+export const Navbar = ({ variant = "transparent" }: NavbarProps) => {
     const isTranparent = variant === "transparent";
-
+    const [isOpen, setIsOpen] = useState(false);
     return (
-    <section className={`top-0 left-0 z-50 w-full 
-        ${isTranparent ?`absolute`: `sticky border-b border-black/5 bg-card`}`}>
-        <div className="mx-auto max-w-7xl px-6 lg:px-12">
-            <nav className={`flex h-20 items-center justify-between
-                ${isTranparent ?`mt-6 rounded-3xl border border-white/10 bg-white/5 px-6 backdrop-blur-2xl`:`px-0 `}`}>
+        <section className={`top-0 left-0 z-50 w-full 
+        ${isTranparent ? `absolute` : `sticky border-b border-black/5 bg-card`}`}>
+            <div className="mx-auto max-w-7xl px-6 lg:px-12">
+                <nav className={`flex h-20 items-center justify-between
+                ${isTranparent ? `mt-6 rounded-3xl border border-white/10 bg-white/5 px-6 backdrop-blur-2xl` : `px-0 `}`}>
                     {/* logo */}
                     <Link href={'/'} className="flex items-center text-2xl font-semibold ">
-                        <span className={`${isTranparent ? `text-gray-300`:`text-text`}`}>
+                        <span className={`${isTranparent ? `text-gray-300` : `text-text`}`}>
                             Next
                         </span>
                         <span className="bg-primary text-white px-2 py-1 rounded-tr-2xl rounded-bl-2xl">
@@ -27,21 +32,60 @@ export const Navbar = ({variant="transparent"}:NavbarProps) => {
                     </Link>
                     {/* Desktop Links */}
                     <div className="hidden items-center gap-8 lg:flex">
-                        {navLinks.map((item)=>(
-                            <Link key={item} href={item=="Home"?"/":`${item.toLowerCase()}`} className={`text-sm font-medium transition hover:text-primary
-                            ${isTranparent?`text-white/80`:`text-white/70`}`}>
+                        {navLinks.map((item) => (
+                            <Link key={item} href={item == "Home" ? "/" : `${item.toLowerCase()}`} className={`text-sm font-medium transition hover:text-primary
+                            ${isTranparent ? `text-white/80` : `text-white/70`}`}>
                                 {item}
                             </Link>
                         ))}
                     </div>
                     {/* Desktop Buttons */}
                     <div className="hidden lg:flex items-center gap-4 ">
-                        <Button>
+                        <Button variant="outline">
                             Login
                         </Button>
+                        <Button icon={<FaHome />} iconPosition="right" variant="outline">
+                            Add Property
+                        </Button>
+
                     </div>
-            </nav>
-        </div>
-    </section>
-  )
+                    {/* Mobile Button */}
+                    <button className={`
+                        flex h-11 w-11 items-center justify-center
+                        rounded-2xl transition lg:hidden
+                        ${isTranparent ? "border border-white/10 bg-white/5 text-white " : "border border-black/10 bg-background text-text"}`}
+                        onClick={() => {
+                            setIsOpen(!isOpen)
+                        }}>
+                        {isOpen ? <IoClose /> : <HiOutlineMenuAlt3 />}
+                    </button>
+
+
+                </nav>
+                {/* Mobile Menu*/}
+                {isOpen && (
+                    <div className={`mt-4 rounded-3xl p-6 backdrop-blue-2xl lg:hidden 
+                        ${isTranparent ? `border border-white/10 bg-secondary/95 ` : `border border-black/5 bg-white`}`}>
+                        <div className="flex flex-col gap-5 ">
+                            {navLinks.map((item) => (
+                                <Link key={item} href={item == "Home" ? "/" : `${item.toLowerCase()}`} className={` transition hover:text-primary
+                                        ${isTranparent ? `text-white/80` : `text-white/70`}`}>
+                                    {item}
+                                </Link>
+                            ))}
+                            <div className="flex flex-col gap-3 mt-4 ">
+                                <Button variant="outline">
+                                    Login
+                                </Button>
+                                <Button icon={<FaHome />} iconPosition="right" variant="outline">
+                                    Add Property
+                                </Button>
+                            </div>
+                        </div>
+
+                    </div>
+                )}
+            </div>
+        </section>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { SearchBox } from "./components/home/SearchBox";
 import FrontendLayout from "./components/layouts/FrontendLayout";
 import { Navbar } from "./components/navbar/Navbar";
 export default function Home() {
@@ -7,10 +8,34 @@ export default function Home() {
       <section className="relative flex min-h-screen items-center overflow-hidden bg-[url('/data/hero.jpg')] bg-cover bg-center
       pt-32 lg:pt-36 py-2">
         {/* overlay */}
-        <div className="absolute inset-0 bg-black/55"/>
+        <div className="absolute inset-0 bg-black/55" />
         {/* gradient */}
-        <div className="absolute inset-0 bg-linear-to-r from-slate-950/80 via-slate-900/50 to-transparent"/> 
-      
+        <div className="absolute inset-0 bg-linear-to-r from-slate-950/80 via-slate-900/50 to-transparent" />
+        {/* contents */}
+        <div className="relative z-10 w-full ">
+          <div className="mx-auto max-w-7xl px-6 lg:px-12">
+            <div className="max-w-3xl">
+              {/* badge */}
+              <div className="mb-6 inline-flex items-center gap-2 rounded-full border border-white/10 bg-white/5 px-5 py-2 backdrop-blur-xl">
+                <div className="w-2 h-2 rounded-full bg-primary" />
+                <span className="text-sm font-medium tracking-wide text-white">
+                  Premium Real Estate MarketPlace
+                </span>
+              </div>
+
+              {/* heading */}
+              <h2 className="text-4xl font-bold leading-tight text-white md:text-6xl lg:text-7xl">
+                Find The Perfect Place to Call Home
+              </h2>
+              <p className="mt-6 max-w-2xl text-lg leading-relaxed text-white/70">
+                Discover luxury apartments, modern homes, and premium
+                properties
+                in the best locations around the world.
+              </p>
+              <SearchBox />
+            </div>
+          </div>
+        </div>
       </section>
     </FrontendLayout>
   );
