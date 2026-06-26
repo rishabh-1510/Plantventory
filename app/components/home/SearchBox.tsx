7e84ecbecce284c8a09b8e2f8ce850fda9ee7715
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-06-26 18:35:07 +0530

    SearchBox

--- app/components/home/SearchBox.tsx
+++ app/components/home/SearchBox.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Button } from '../ui/Button'
 
 export const SearchBox = () => {
   return (
-    <div>SearchBox</div>
+    <div className='mt-10 rounded-[30px] border border-white/10 p-4 shadow-2xl backdrop-blur-2xl '>
+      <div className='flex flex-col gap-4 lg:flex-row items-center'>
+        <input type='text' placeholder='Search by city , neighborhood , or address' className='px-5 py-5 h-14 w-full flex-1 rounded-2xl border bg-white/5
+              text-white
+              placeholder:text-white/50
+                outline-none transition
+              focus:border-primary/40 >'/>
+              <Button>
+                Search Properties
+              </Button>
+      </div>
+    </div>
   )
 }
